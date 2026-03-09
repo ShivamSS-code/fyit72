881bad612e0011edc090ccb5067393801e7fa9ce
ShivamSS-code <sharmashivam56812@gmail.com>
2026-03-09 11:30:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+function showPopup()
+{
+alert("Welcome to my Portfolio Website!");
+}
+
+function validateForm(){
+
+let name = document.getElementById("name").value;
+let email = document.getElementById("email").value;
+let message = document.getElementById("message").value;
+
+if(name == "" || email == "" || message == ""){
+alert("Please fill all fields");
+return false;
+}
+
+alert("Form submitted successfully!");
+return true;
+
+}
